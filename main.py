2fb2475da4a033da40aa1b0aa94d176718acb77a
petruskadominik <petruskadominik@gmail.com>
2024-10-13 11:23:56 +0200

    First steps with making the game, opening game screen filled with solid black colour at the moment

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+       for event in pygame.event.get(): # Check for player trying to use X button to close the game and quits game if pressed
+            if event.type == pygame.QUIT:
+                return
+       pygame.Surface.fill(screen, (0, 0, 0)) # Make a black screen for the game
+       pygame.display.flip()    # Refresh game screen
 
 
 
